c5a376039ab4d1761d549b5a5e1cb3385b37d84c
AyshaUrmi0 <ayshaismail021@gmail.com>
2024-11-18 12:49:45 +0900

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBiSH3kX2MXYczud69W3NTa53-WFNyZ8-4",
+  authDomain: "winter-kindness-hub.firebaseapp.com",
+  projectId: "winter-kindness-hub",
+  storageBucket: "winter-kindness-hub.firebasestorage.app",
+  messagingSenderId: "526038799162",
+  appId: "1:526038799162:web:61364e0cedd2d925a13603"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
